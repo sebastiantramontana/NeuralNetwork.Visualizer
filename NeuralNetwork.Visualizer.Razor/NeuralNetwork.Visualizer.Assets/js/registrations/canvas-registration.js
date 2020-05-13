0cd0e4e58fb2684b6f8bc78cf8b337be82656ab7
Sebastian <sebastiantramontana@gmail.com>
2020-05-13 18:23:23 -0300

    Begin and End drawing in canvas-registration.js

--- NeuralNetwork.Visualizer.Razor/NeuralNetwork.Visualizer.Assets/js/registrations/canvas-registration.js
+++ NeuralNetwork.Visualizer.Razor/NeuralNetwork.Visualizer.Assets/js/registrations/canvas-registration.js
@@ -1,3 +1,31 @@
 ﻿var registerCanvasDomAccess = registerCanvasDomAccess || ((globalInstanceName) => {
-    console.log("Canvas registered!")
+
+    const canvasId = 'neuralnetwork-visualizer-canvas-' + globalInstanceName;
+
+    let offscreenCanvas = nul;
+    let currentContext = null;
+
+    getCanvasElement = () => {
+        const canvas = document.getElementById(canvasId);
+        return canvas;
+    };
+
+    beginDrawing = () => {
+        canvas = getCanvasElement();
+
+        offScreenCanvas = new OffscreenCanvas(canvas.offsetWidth, canvas.offsetHeight);
+        currentContext = offScreenCanvas.getContext("2d");
+    };
+
+    endDrawing = () => {
+        const canvas = getCanvasElement();
+        const bitmap = offscreenCanvas.transferToImageBitmap();
+
+        canvas.transferFromImageBitmap(bitmap);
+    }
+
+    window[globalInstanceName].Canvas = {
+
+       
+    };
 });
